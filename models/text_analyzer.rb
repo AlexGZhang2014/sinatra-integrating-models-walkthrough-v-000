be02de48cbd7f7180cf19b69b1a1aece9ec453e1
Alex Zhang <alexgzhang92@gmail.com>
2018-06-22 01:03:34 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,3 +1,15 @@
 class TextAnalyzer
+  attr_reader :text
   
+  def initialize(text)
+    @text = text.downcase
+  end
+  
+  def count_of_words
+    words = text.split(" ")
+    words.count
+  end
+  
+  def count_of_vowels
+    
 end
